4fb9d6411d359a6ed4efb85284b27ee2a9faa97b
Diwakar Gautam <diwakargautam823@gmail.com>
2025-03-05 18:29:31 +0530

    edited on 5Mar 18:30

---
+++ src/db/index.js
@@ -0,0 +1,33 @@
+// import mongoose from "mongoose";
+// import { DB_NAME } from "../constants";
+
+// const connectDB = async () => {
+//     try{
+//          const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+//          console.log(`\n MongoDb connected !! DB HOST: ${connectionInstance.connection.host}`)
+//     }
+//     catch(error){
+//        console.log("MONGODB connection error ",error);
+//        process.exit(1)
+//     }
+// }
+
+// export default connectDB
+
+
+
+import mongoose from "mongoose";
+import {DB_NAME} from "../constants";
+
+const connectDB = async() => {
+    try{
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        console.log(`MongoDB connected Successfully !! DB HOST: ${connectionInstance.connection.host}`)
+    }
+    catch(error){
+        console.log("MongoDB connection error ",error)
+        process.exit(1)
+    }
+}
+
+export default connectDB
